355ab44865b1dbeee5a673789b3ca324aaf6c3f2
ruslanator <latyshevruslan@ya.ru>
2021-07-14 23:58:35 +0300

    spring-data-app

---
+++ spring-data-app/src/main/resources/db/migration/V1__createDB.sql
@@ -0,0 +1,7 @@
+create table products (id bigserial primary key, title varchar(255), cost int);
+insert into products (title, cost) values
+('Milk', 55),
+('Cheese', 90),
+('Banana', 15),
+('Pork', 110),
+('Apple', 10);
